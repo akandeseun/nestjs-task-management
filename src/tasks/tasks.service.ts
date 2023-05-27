ead1501df39de43dadf096d99753cf8fd6bb430e
akandeseun <akandeseun44@gmail.com>
2023-05-27 12:48:38 +0100

    customPipe

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, NotFoundException } from "@nestjs/common"
 import { Task, TaskStatus } from "./tasks.model"
 import { v4 as uuidv4 } from "uuid"
 import { CreateTaskDto } from "./dto/create-task.dto"
@@ -31,7 +31,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id)
+    const found = this.tasks.find((task) => task.id === id)
+
+    if (!found) {
+      throw new NotFoundException(`No Task found with id: ${id}`)
+    }
+    return found
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -48,18 +53,19 @@ export class TasksService {
   }
 
   updateTask(id: string, status: TaskStatus): Task {
-    const task = this.tasks.find((task) => task.id === id)
+    const task = this.getTaskById(id)
     task.status = status
     return task
   }
 
   deleteTask(id: string): Task[] {
-    const task = this.tasks.find((task) => task.id === id)
+    const task = this.getTaskById(id)
     const taskIndex = this.tasks.indexOf(task)
     return this.tasks.splice(taskIndex)
   }
 
   deleteTaskAlternative(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id)
+    const found = this.getTaskById(id)
+    this.tasks = this.tasks.filter((task) => task.id !== found.id)
   }
 }
