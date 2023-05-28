7d17fcfde0beaf46f3d07a1bd3f48d9d8fdf1a0b
akandeseun <akandeseun44@gmail.com>
2023-05-28 09:38:10 +0100

    refactor to repository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,10 +1,24 @@
 import { Task } from "./task.entity"
 import { Repository, DataSource } from "typeorm"
 import { Injectable } from "@nestjs/common"
+import { CreateTaskDto } from "./dto/create-task.dto"
+import { TaskStatus } from "./task-status.enum"
 
 @Injectable()
 export class TaskRepository extends Repository<Task> {
   constructor(private dataSource: DataSource) {
     super(Task, dataSource.createEntityManager())
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto
+
+    const task = new Task()
+    task.title = title
+    task.description = description
+    task.status = TaskStatus.OPEN
+    await task.save()
+
+    return task
   }
 }
