af4e1529b6781ba337e4ace01799a78406155639
akandeseun <akandeseun44@gmail.com>
2023-05-30 13:35:11 +0100

    handle duplicates and hashing

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,12 @@
 import { DataSource, Repository } from "typeorm"
 import { User } from "./user.entity"
-import { Injectable } from "@nestjs/common"
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from "@nestjs/common"
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto"
+import * as bcrypt from "bcrypt"
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -12,10 +17,24 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto
 
+    const salt = await bcrypt.genSalt()
+    console.log(salt)
+    // const exists = this.findOneBy({ username })
+    // if (exists) {
+    //   throw new BadRequestException()
+    // }
+
     const user = new User()
     user.username = username
     user.password = password
 
-    await user.save()
+    try {
+      await user.save()
+    } catch (error) {
+      if (error.code === "23505") {
+        throw new ConflictException("Username already exists")
+      }
+      throw new InternalServerErrorException("Something went wrong")
+    }
   }
 }
