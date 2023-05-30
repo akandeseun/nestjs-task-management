6ccb9331c1461fad461b03c02b667e6112d204db
akandeseun <akandeseun44@gmail.com>
2023-05-30 12:03:09 +0100

    persist

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("signup")
-  signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
-    console.log(authCredentialsDto)
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto)
   }
 }
