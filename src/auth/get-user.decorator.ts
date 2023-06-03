310c7af94bfaaad52d84f59b57591d9b88fb06fb
akandeseun <akandeseun44@gmail.com>
2023-06-03 13:52:39 +0100

    auth test

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,3 +1,6 @@
 import { createParamDecorator } from "@nestjs/common"
+import { User } from "./user.entity"
 
-const GetUser = createParamDecorator((data, req) => [])
+const GetUser = createParamDecorator((data, req): User => {
+  return req.user
+})
