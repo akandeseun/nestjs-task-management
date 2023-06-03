c1829d9c6a906bacda881d49baa6a8652f018a15
akandeseun <akandeseun44@gmail.com>
2023-06-03 14:21:07 +0100

    implement guard

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,6 +11,7 @@ import {
   ValidationPipe,
   ParseIntPipe,
   HttpCode,
+  UseGuards,
 } from "@nestjs/common"
 import { TasksService } from "./tasks.service"
 import { CreateTaskDto } from "./dto/create-task.dto"
@@ -18,8 +19,10 @@ import { GetTaskFilterDto } from "./dto/get-tasks-filter.dto"
 import { TaskStatusValidationPipe } from "./pipes/task-status-validation.pipe"
 import { Task } from "./task.entity"
 import { TaskStatus } from "./task-status.enum"
+import { AuthGuard } from "@nestjs/passport"
 
 @Controller("tasks")
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
